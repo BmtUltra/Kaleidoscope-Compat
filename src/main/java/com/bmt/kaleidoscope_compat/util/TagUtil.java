d2722fdcfc7dcaddf1155d53f6663880e29c9ae4
BmtUltra <1453935889@qq.com>
2026-03-28 08:27:11 +0800

    更好的兼容性

--- src/main/java/com/bmt/kaleidoscope_compat/util/TagUtil.java
+++ src/main/java/com/bmt/kaleidoscope_compat/util/TagUtil.java
@@ -8,15 +8,31 @@ import net.minecraft.world.level.block.Block;
 
 public class TagUtil {
     public static class Items {
+        /**
+         * 草帽饰品化
+         * 用于标记那些可以被当成草帽的饰品
+         */
         public static final TagKey<Item> STRAW_HATS =
                 TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath("kaleidoscope_cookery", "straw_hat"));
 
+        /**
+         * 正常瓶子的酒
+         * 用于标记那些可以被放入森罗酒柜的方块
+         */
         public static final TagKey<Item> SMALL_BOTTLE =
                 TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath("kaleidoscope_tavern", "small_bottle"));
 
+        /**
+         * 异形瓶子的酒
+         * 但是我还没做完
+         */
         public static final TagKey<Item> LARGE_BOTTLE =
                 TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath("kaleidoscope_tavern", "large_bottle"));
 
+        /**
+         * 幻想乡的酒柜
+         * 用于标记那些可以被放入幻想乡酒柜的方块
+         */
         public static final TagKey<Item> WINE =
                 TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath("youkaisfeasts", "wine"));
     }
