a3349a786a2a25c552efffcf97d6aa8d3c4c2202
BmtUltra <1453935889@qq.com>
2026-06-06 11:53:11 +0800

    icon

--- src/main/java/com/bmt/kaleidoscope_compat/mixins/kaleidoscope_cookery/InitMixin.java
+++ src/main/java/com/bmt/kaleidoscope_compat/mixins/kaleidoscope_cookery/InitMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(KaleidoscopeCookery.class)
-public class RegistryMixin {
+public class InitMixin {
 
     @Inject(
         method = "<init>",
